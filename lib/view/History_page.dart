c827febd792d11c64849a539916709ad31e50553
NhanHub <duynhan127nvc@gmail.com>
2022-11-25 15:49:57 +0700

    LichSuDau_Complete

--- lib/view/History_page.dart
+++ lib/view/History_page.dart
@@ -351,6 +351,102 @@ class _History extends State<History> {
                   width: 700,
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+                        child: Text(
+                          'Màn 2:',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        'Số câu trả lời đúng ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        '3/5',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  height: 50,
+                  width: 700,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+                        child: Text(
+                          'Màn 2:',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        'Số câu trả lời đúng ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        '3/5',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  height: 50,
+                  width: 700,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+                        child: Text(
+                          'Màn 2:',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        'Số câu trả lời đúng ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        '3/5',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  height: 50,
+                  width: 700,
+                ),
+              ),
             ],
           ),
         ),
