481e181bb08cc9ca6c09d56b5df6aca58b6f245b
huybo1410 <tuilahuy69@gmail.com>
2022-11-25 15:21:48 +0700

    TaoTrangLichSu

---
+++ lib/view/History_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class History extends StatefulWidget {
+  const History({super.key});
+  @override
+  State<History> createState() => _History();
+}
+
+
+class _History extends State<History> {
+ 
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(),
+      body: 
+      
+    );
+  }
+}
